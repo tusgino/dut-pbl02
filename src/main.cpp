f9b3a82f4e2d887611c303832c41fbb24eb625d5
Tugino <nguyendinhtu.job@gmail.com>
2022-11-29 13:46:46 +0700

    2.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -19,9 +19,18 @@ int main()
   // QuanLi *p;
   // p = new QLKhachHang();
 
-  QLKhachHang *qlkh = new QLKhachHang();
+  // QLKhachHang *qlkh = new QLKhachHang();
   fstream temp;
   temp.open("components/data/ola.dat", ios_base::in);
-  qlkh->docFile(temp);
+  // qlkh->docFile(temp);
+
+  QuanLi *p = new QLKhachHang();
+  p->docFile(temp);
+  temp.close();
+  fstream temp1;
+  temp1.open("components/data/ola1.dat", ios_base::out);
+  p->ghiFile(temp1);
+  temp1.close();
+
   return 0;
 }
