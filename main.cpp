66cdbb8c998d1d20bb4c386e20faccb424e24609
DangHuan2k3 <huan.bk.0412@gmail.com>
2022-11-30 14:13:45 +0700

    2.0.0

--- main.cpp
+++ main.cpp
@@ -1,25 +1,26 @@
 // #include "features\SanPham\SanPham.cpp"
 // #include "src\components\utils\Ma.cpp"
-#include "src\features\QuanLi\QLKhachHang.cpp"
+// #include "src\features\QuanLi\QLKhachHang.cpp"
+#include "src\features\QuanLi\QLSanPham.cpp"
 
 using namespace std;
 
 int main()
 {
-  // QLSanPham *p = new QLSanPham();
-  // return 0;
-  QLKhachHang *p = new QLKhachHang();
-  fstream fileKH;
-  fileKH.open("src/components/data/ola.dat", ios_base::in);
-  p->docFile(fileKH);
-  fileKH.close();
+  QLSanPham *p = new QLSanPham();
+  return 0;
+  // QLKhachHang *p = new QLKhachHang();
+  // fstream fileKH;
+  // fileKH.open("src/components/data/ola.dat", ios_base::in);
+  // p->docFile(fileKH);
+  // fileKH.close();
       // p->create();
   // p->read();
-  p->deleteIndex();
+  // p->deleteIndex();
   // p->find();
-  fileKH.open("src/components/data/ola1.dat", ios_base::out);
-  p->ghiFile(fileKH);
-  fileKH.close();
+  // fileKH.open("src/components/data/ola1.dat", ios_base::out);
+  // p->ghiFile(fileKH);
+  // fileKH.close();
   cout << 1;
 
   return 0;
